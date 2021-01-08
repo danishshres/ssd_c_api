d6275a102ef2a679315fe5291ca953705dce40b3
danishshres <danishshres@gmail.com>
2021-01-08 23:51:50 +0100

    Compatible with tf 2x versions

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -8,6 +8,8 @@
 
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
+#include<opencv2/core/types.hpp>
+#include<opencv2/imgproc.hpp>
 
 void DeallocateBuffer(void* data, size_t) {
 	std::free(data);
@@ -54,7 +56,7 @@ class Network{
 		void Run(cv::Mat  image_record);
 		static void Deallocator(void* data, size_t length, void* arg);
 		Network();
-
+		~Network();
 	private:
 		TF_Session* session;
 		TF_Graph* graph;
@@ -67,32 +69,30 @@ Network::Network()
 
 void Network::LoadGraph(std::string modelPath)
 {
-	TF_Buffer* buffer = ReadBufferFromFile(modelPath);
-	if (buffer == nullptr) {
-		throw std::invalid_argument("Error creating the session from the given model path %s !");
-	}
-	TF_Status* status = TF_NewStatus();
-	TF_ImportGraphDefOptions* opts = TF_NewImportGraphDefOptions();
-
 	graph = TF_NewGraph();
-	TF_GraphImportGraphDef(graph, buffer, opts, status);
-	TF_DeleteImportGraphDefOptions(opts);
-	TF_DeleteBuffer(buffer);
-	if (TF_GetCode(status) != TF_OK) {
-		TF_DeleteGraph(graph);
-		graph = nullptr;
-	}
-	TF_DeleteStatus(status);
+	TF_Status* Status = TF_NewStatus();
+	TF_SessionOptions* SessionOpts = TF_NewSessionOptions();
+	TF_Buffer* RunOpts = NULL;
+	const char* tags = "serve";
+	int ntags = 1;
+	char* path = const_cast<char*>(modelPath.c_str());
 
-	// create session from graph
-	status = TF_NewStatus();
-	TF_SessionOptions* options = TF_NewSessionOptions();
-	session = TF_NewSession(graph, options, status);
-	TF_DeleteSessionOptions(options);
+	session = TF_LoadSessionFromSavedModel(SessionOpts, RunOpts, path, &tags, ntags, graph, NULL, Status);
+	if (TF_GetCode(Status) == TF_OK)
+	{
+		printf("Tensorflow 2x Model loaded OK\n");
+	}
+	else
+	{
+		printf("%s", TF_Message(Status));
+	}
+	TF_DeleteSessionOptions(SessionOpts);
+	TF_DeleteStatus(Status);
 }
 
 void Network::Run(cv::Mat image)
 {
+
 	std::vector<TF_Output> 	input_tensors, output_tensors;
 	std::vector<TF_Tensor*> input_values, output_values;
 
@@ -101,16 +101,30 @@ void Network::Run(cv::Mat image)
 	std::int64_t input_dims[4] = {1, image.rows, image.cols, 3}; //1 is number of batch, and 3 is the no of channels.
 	int num_bytes_in = image.cols * image.rows * 3; //3 is the number of channels.
 	
-	input_tensors.push_back({TF_GraphOperationByName(graph, "image_tensor"),0});
+	TF_Output t0 = { TF_GraphOperationByName(graph, "StatefulPartitionedCall"),0};
+	if (t0.oper == NULL)
+		printf("ERROR: Failed TF_GraphOperationByName StatefulPartitionedCall\n");
+	else
+		printf("TF_GraphOperationByName StatefulPartitionedCall is OK\n");
+
+	size_t pos = 0;
+	TF_Operation* oper;
+	
+	while ((oper = TF_GraphNextOperation(graph, &pos)) != nullptr) {
+		printf(TF_OperationName(oper));
+		printf("\n");
+	}
+	input_tensors.push_back({TF_GraphOperationByName(graph, "serving_default_input_tensor"),0});
 	input_values.push_back(TF_NewTensor(TF_UINT8, input_dims, num_dims, image.data, num_bytes_in, &Deallocator, 0));
 
-	output_tensors.push_back({ TF_GraphOperationByName(graph, "detection_classes"),0 });
+
+	output_tensors.push_back({ TF_GraphOperationByName(graph, "StatefulPartitionedCall"),0});
 	output_values.push_back(nullptr);
 
-	output_tensors.push_back({ TF_GraphOperationByName(graph, "detection_scores"),0 });
+	output_tensors.push_back({ TF_GraphOperationByName(graph, "StatefulPartitionedCall"),2 });
 	output_values.push_back(nullptr);
 
-	output_tensors.push_back({ TF_GraphOperationByName(graph, "detection_boxes"),0 });
+	output_tensors.push_back({ TF_GraphOperationByName(graph, "StatefulPartitionedCall"),3 });
 	output_values.push_back(nullptr);
 
 	TF_Status* status = TF_NewStatus();
@@ -118,46 +132,88 @@ void Network::Run(cv::Mat image)
 		&input_tensors[0], &input_values[0], input_values.size(),
 		&output_tensors[0], &output_values[0], 3, //3 is the number of outputs count..
 		nullptr, 0, nullptr, status
-	);
-	if (TF_GetCode(status) != TF_OK)
+	);	if (TF_GetCode(status) != TF_OK)
 	{
 		printf("ERROR: SessionRun: %s", TF_Message(status));
 	}
-	
-	auto detection_classes = static_cast<float_t*>(TF_TensorData(output_values[0]));
-	auto detection_scores = static_cast<float_t*>(TF_TensorData(output_values[1]));
-	auto detection_boxes = static_cast<float_t*>(TF_TensorData(output_values[2]));
-
+	auto detection_classes = static_cast<float_t*>(TF_TensorData(output_values[1]));
+	auto detection_scores = static_cast<float_t*>(TF_TensorData(output_values[2]));
+	auto detection_boxes = static_cast<float_t*>(TF_TensorData(output_values[0]));
 
 	for (int i = 0; i < 6; i++) { //6 is not max number of detections of the network.
 		std::cout << "Class Id: "<<detection_classes[i] <<" ";
 		std::cout << "Confidence: " << detection_scores[i] << " ";
 		for (int j = 0; j < 4; j++) {
-			std::cout << "Cordinates: " << detection_boxes[i] << " ";
+			std::cout << "Cordinates: " << detection_boxes[1 * i + j] << " ";
 		}
 		std::cout << "\n";
 	}
-	std::cout << detection_classes;
+	std::cout << detection_boxes;
+
+	cv::Rect rect = cv::Rect(detection_boxes[1] * image.cols, detection_boxes[0] * image.rows, (detection_boxes[3] - detection_boxes[1]) * image.cols, (detection_boxes[2] - detection_boxes[0]) * image.rows);
+	rectangle(image, rect, cv::Scalar(0, 255, 0), 2, 8, 0);
+	cv::imshow("test", image);
+	cv::waitKey();
+
+	
+	//free memory
+	TF_DeleteStatus(status);
+
+	for (auto& t : output_values) {
+		TF_DeleteTensor(t);
+	}
+	output_values.clear();
+	output_values.shrink_to_fit();
+
+	
+	for (auto& t : input_values) {
+		TF_DeleteTensor(t);
+	}
+
+	input_values.clear();
+	input_values.shrink_to_fit();
+
+	input_tensors.clear();
+	input_tensors.shrink_to_fit();
+
 }
 
 void Network::Deallocator(void* data, size_t length, void* arg)
 {
-	std::free(data);
+	printf("Dellocation of the input tensor");
+	//std::free(data);
+	//data = nullptr;
 }
+
+Network::~Network()
+{
+	TF_DeleteGraph(graph);
+	TF_Status* status = TF_NewStatus();
+	TF_CloseSession(session, status);
+	if (TF_GetCode(status) != TF_OK) {
+		printf("Error close session");
+	}
+	TF_DeleteSession(session, status);
+	TF_DeleteStatus(status);
+}
+
+
 
 int main()
 {
-	std::string modelPath = "D:/Projects/1_HAZMAT/trunk/PVRE_DE_SICK_HAZMAT/bin/PVRE/ProtobufFiles/HAZMAT_detector_binary_20180914.pb";
-	std::string imagePath = "D:/Images/Hazmat/SICK/2018_07_19_01_Single_Labels/01_Single_Labels/12.x/12.1/0d886616ccfbc4911d36b7ca731dcb1c778a22d0.jpg";
+	std::string modelPath = "D:/Projects/Tensorflow_C_Api_Ssd/tf_2x/models/centernet_resnet50_v2_512x512_coco17_tpu-8_v2/saved_model";
+	std::string imagePath = "D:/Projects/Tensorflow_C_Api_Ssd/tf_2x/20201007214411187_4239.png";
 	
 	Network ssd_network;
 	ssd_network.LoadGraph(modelPath);
 
 	cv::Mat image;
 	image = cv::imread(imagePath);
-	//cv::imshow("test", image);
-	//cv::waitKey();
 	image.convertTo(image, CV_8UC3);
 	//std::cout << image.channels;
-	ssd_network.Run(image);
+	for (size_t i = 0; i < 100; i++) {
+		ssd_network.Run(image);
+	}
+	ssd_network.~Network();
+	image = NULL;
 }
